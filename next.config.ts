09f9cefaac5fe30769f300b7c180f07eca8705d1
Kebin <keninadod@gmail.com>
2026-08-21 18:43:00 +0545

    chore: remove deprecated eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-﻿import type {NextConfig} from 'next';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: false,
   },
